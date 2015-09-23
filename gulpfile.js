ef975cbc2462d7d0b93e1a15c295756c8e67cb6d
YuTanaka <YRK00337@nifty.com>
2015-09-23 22:05:35 +0900

    Commit from gulp.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// gitコミット
+gulp.task('git', function() {
+  var comm = 'Commit from gulp.';
+  for (var i=0 ; i<process.argv.length-1 ; i++) {
+    if (process.argv[i] == '--m') {
+      comm = process.argv[i+1];
+    }
+  }
+  return gulp.src('.')
+    .pipe($.git.add())
+    .pipe($.git.commit(comm));
+});
+
+// ローカルで更新
+gulp.task('local', ['git'], function() {
+  shell.task(
+    ['heroku local web']
+  );
+  browserSync({
+    notify: false,
+    logPrefix: 'BS',
+    server: ['localhost:5000']
+  });
+});
+
+gulp.task('default', ['local']);
