9635e0804b6964f82778663b5bb2f7cad8c16a87
YuTanaka <YRK00337@nifty.com>
2015-09-25 16:08:35 +0900

    check type.

--- routes/index.js
+++ routes/index.js
@@ -53,7 +53,7 @@ router.post('/', upload.single('filePhoto'), function(req,res,next) {
       // 読み込みイベントを設定
       stdout.on('data', function(chunk) {
         datas.push(datas);
-        console.log(chunk.length);
+        console.log(chunk.length+"/"+typeof(chunk));
       }).on('end', function (chunk) {
         if (chunk) {datas.push(chunk);}
         var img = datas.join('');
