b4b4be343b78e733b66c40539c0aca16e2e4e16b
YuTanaka <YRK00337@nifty.com>
2015-09-25 13:35:40 +0900

    change to fullpath.

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.post('/', upload.single('filePhoto'), function(req,res,next) {
   var photo = req.file;
   var photopath = join(__dirname, photo.path);
   //photopath = __dirname+'/nodejs.png';
-  photopath = photo.path;
+  photopath = join('/Users/yutanaka/git/photo-sender/',photo.path);
 
   console.log('file:'+photopath);
 
