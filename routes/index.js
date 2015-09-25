f99b9c9429d987244bd1293d3acda72d47d23a3e
YuTanaka <YRK00337@nifty.com>
2015-09-25 13:25:01 +0900

    back to upload file.

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.post('/', upload.single('filePhoto'), function(req,res,next) {
   }
   var photo = req.file;
   var photopath = join(__dirname, photo.path);
-  photopath = __dirname+'/nodejs.png';
+  //photopath = __dirname+'/nodejs.png';
 
   // 画像縮小
   var resizeX = 343;
