41a982caaf74a7bb7124929de1e6cc89c3a1f224
YuTanaka <YRK00337@nifty.com>
2015-09-25 18:36:15 +0900

    size test2.

--- routes/index.js
+++ routes/index.js
@@ -52,6 +52,9 @@ router.post('/', upload.single('filePhoto'), function(req,res,next) {
       // 画像縮小
       var datas = [];
       var base = imageMagick(photopath);
+      base.size().forEach(function(name) {
+        console.log(name);
+      });
       console.log('size='+base.size());
       base.resize(resizeX, resizeY)
         .write(destpath, function(err) {
